6e91e988efcb4ae7446adb0eb45fd426423a4b7c
Max Lapan <max.lapan@gmail.com>
2009-04-18 23:25:34 +0400

    Removed unnecessary third-party libraries.

--- sqlite-fake.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage :sqlite-fake
-  (:use :cl)
-  (:export :sqlite-open))
-
-(in-package :sqlite-fake)
-
-
-(defun sqlite-open (name &optional (mode 0))
-  (format t "sqlite-fake: open ~a (~d)~%" name mode))
--- thttp/tests/package.lisp
+++
@@ -1,4 +0,0 @@
-(in-package #:common-lisp-user)
-
-(defpackage #:trivial-http-test
-  (:use #:common-lisp #:lift #:trivial-http))
